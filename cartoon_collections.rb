c252fa92d7c4d295e0f1e469621f987a82f61c2e
Goldie <goldiejackson25@yahoo.com>
2021-12-02 06:13:18 -0600

    complete

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.map.with_index(1) {|x, index| puts "#{index}. #{x}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  new = planeteer_calls.map {|x| x.capitalize}
+  new.map {|x| "#{x}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  x = calls.filter {|c| c.length > 4 }
+  if x.length > 0
+    return true
+  else
+    return false
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese string_array
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  intersect = string_array & cheese_types
+  intersect[0]
 end
